b4b5ebed2076ae3e5efba4c23af3bd108bb0957b
Chamz <chamzzzzzz@gmail.com>
2013-02-27 00:20:04 +0800

    Add _dopremake _domake _dodeploy 3 pharse of build of premake4.lua Todo more output of premake4 when action

--- premake4.lua
+++ premake4.lua
@@ -1,5 +1,7 @@
 
 
+
+
 if not _ACTION then
   printf("_ACTION is nil!")
   return
@@ -28,12 +30,12 @@ project "lolicore"
   }
 
   configuration "debug"
-    targetdir "_bin/debug"
+    targetdir ("_bin/" .. _ACTION .. "/debug")
     defines "_DEBUG"
     flags { "Symbols" }
 
   configuration "release"
-    targetdir "_bin/release"
+    targetdir ("_bin/" .. _ACTION .. "/release")
     defines "NDEBUG"
     flags { "OptimizeSize" }
 
@@ -70,3 +72,100 @@ if _ACTION == "clean" then
   os.rmdir("_build")
   os.rmdir("_deploy")
 end
+
+local function exec(cmd, ...)
+  cmd = string.format(cmd, unpack(arg))
+  local z = os.execute(cmd .. " > output.log 2> error.log")
+  --local z = os.execute(cmd)
+  os.remove("output.log")
+  os.remove("error.log")
+  return z
+end
+
+local function _dopremake()
+  local action = _OPTIONS["action"] or "gmake"
+  printf("Premaking %s...", action)
+  exec("premake4 %s", action)
+end
+
+local function _domake()
+  local action = _OPTIONS["action"] or "gmake"
+  local config = _OPTIONS["config"] or "debug"
+  printf("Making %s %s...", action, config)
+  if action == "gmake" then
+    local cwd = os.getcwd()
+    printf("Current working directory:%s", cwd)
+    os.chdir(string.format("_build/%s", action))
+    exec("make config=%s", config)
+    os.chdir(cwd)
+  else
+    printf("Unsupported action %s now!", action)
+  end
+end
+
+local function _dodeploy()
+  local action = _OPTIONS["action"] or "gmake"
+  local config = _OPTIONS["config"] or "debug"
+
+  printf("Deploy %s %s...", action, config)
+  local bin = string.format("_bin/%s/%s", action, config)
+  if not os.isdir(bin) then
+    printf("%s is not a dir")
+    return
+  end
+
+  os.rmdir("_deploy")
+  os.mkdir("_deploy")
+  local src = string.format("%s/lolicore.exe", bin)
+  local dest = string.format("_deploy/lolicore.exe")
+  if not os.isfile(src) then
+    printf("%s is not a file")
+    return
+  end
+  os.copyfile(src, dest)
+end
+
+newaction
+{
+  trigger = "premake",
+  description = "Premake, Generate native make",
+  execute = _dopremake,
+}
+
+newaction
+{
+  trigger = "make",
+  description = "Make",
+  execute = _domake,
+}
+
+
+newaction
+{
+  trigger = "deploy",
+  description = "Deploy",
+  execute = _dodeploy,
+}
+
+newoption
+{
+  trigger = "config",
+  value = "configvalue",
+  description = "configuration",
+  allowed =
+  {
+    { "debug", "Debug" },
+    { "release", "Release" }
+  }
+}
+
+newoption
+{
+  trigger = "action",
+  value = "actionvalue",
+  description = "Same as _ACTION",
+  allowed =
+  {
+    { "gmake", "gmake" },
+  }
+}
