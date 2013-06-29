3115dde7b4c20733074c36085aadef151e26ca94
Chamz <chamzzzzzz@gmail.com>
2013-06-29 14:11:08 +0800

    Mod: startdev.sh and stopdev.sh to start and stop all server

--- src/sh/stopdev.sh
+++ src/sh/stopdev.sh
@@ -6,5 +6,10 @@ echo "path to script's dir is: [${basedir}]"
 cd ${basedir}
 echo "change working dir to $(pwd)"
 
-#stop god server
+#顺序问题以后再来解决
+#stop servers
+kill -s INT $(cat sa.pid)
+kill -s INT $(cat login.pid)
+kill -s INT $(cat area.pid)
+kill -s INT $(cat gov.pid)
 kill -s INT $(cat god.pid)
