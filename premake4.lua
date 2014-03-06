fa1c3feff5c46d1d4cf983d3e71c92f8b974a14a
Chamz <chamzzzzzz@gmail.com>
2014-03-06 19:39:34 +0800

    Changed: make a hard decision, to give up the support of old Lua

--- premake4.lua
+++ premake4.lua
@@ -27,7 +27,7 @@ solution "lolitall"
   --IS this vs used only?
   debugdir ("_deploy")
   debugargs { "tracelv=0", "X=../test/config_server" }
-  defines { "LUA_COMPAT_ALL" }
+  --defines { "LUA_COMPAT_ALL" }
 
   configuration "debug"
     targetdir ("_bin/" .. _ACTION .. "/debug")
